1b5bbc2131d8902b0a754eb3aa666713eb8d943e
Alec Thomas <alec@swapoff.org>
2013-07-02 21:22:31 -0400

    @provide SQLAlchemy Engine.

--- waffle/db.py
+++ waffle/db.py
@@ -1,6 +1,7 @@
 import logging
 
 from injector import Module, inject, provides, singleton
+from sqlalchemy.engine import Engine as DatabaseEngine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import AbstractConcreteBase, declarative_base, declared_attr
 from sqlalchemy import create_engine
@@ -39,14 +40,20 @@ class DatabaseModule(Module):
     """
 
     @singleton
-    @provides(DatabaseSession)
+    @provides(DatabaseEngine)
     @inject(database_uri=Flag('database_uri'), database_logging=Flag('database_logging'), log_level=LogLevel)
-    def provide_db_session(self, database_uri, database_logging, log_level):
+    def provide_db_engine(self, database_uri, database_logging, log_level):
         assert database_uri, '--database_uri not set, set a default in main() or run()'
         if database_logging:
             logging.getLogger('sqlalchemy.engine').setLevel(log_level)
         logging.info('Connecting to %s', database_uri)
         engine = create_engine(database_uri, convert_unicode=True)
+        return engine
+
+    @singleton
+    @provides(DatabaseSession)
+    @inject(engine=DatabaseEngine)
+    def provide_db_session(self, engine):
         session = scoped_session(sessionmaker(autocommit=False,
                                               autoflush=False,
                                               bind=engine))
